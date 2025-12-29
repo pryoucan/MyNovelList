27b0d685f9f7360b7578afc4dcb3f68e21312f53
unknown <priyanshu123543@gmail.com>
2025-12-29 18:03:44 +0530

    correct usernovel controllers logic, improve zod validation

--- src/controllers/user-novel.controller.js
+++ src/controllers/user-novel.controller.js
@@ -1,25 +1,23 @@
 import { GlobalNovel } from "../models/global-novel.model.js";
-import { UserNovel } from "../models/user-novel.model.js"
+import { UserNovel } from "../models/user-novel.model.js";
 
 const viewNovel = async (req, res) => {
   try {
     const userId = req.user.id;
-    const novels = await UserNovel.find({ user: userId })
+    const novels = await UserNovel.find({ user: userId });
 
     if (novels.length === 0) {
       return res.status(404).json({ message: "Novel is empty" });
     }
     return res.status(200).json({
       message: "Novels fetched successfully",
-      novels
+      novels,
     });
-  }
-  catch (error) {
+  } catch (error) {
     console.log(error);
     return res.status(500).json({ message: "Something went wrong" });
   }
 };
-
 
 const getUserNovel = async (req, res) => {
   try {
@@ -41,13 +39,11 @@ const getUserNovel = async (req, res) => {
       message: "Novel found successfully",
       novel,
     });
-  }
-  catch (error) {
+  } catch (error) {
     console.log(error);
     return res.status(500).json({ message: "Something went wrong" });
   }
 };
-
 
 const searchNovelByName = async (req, res) => {
   try {
@@ -72,13 +68,11 @@ const searchNovelByName = async (req, res) => {
       message: "Novel found successfully",
       novels,
     });
-  }
-  catch (error) {
+  } catch (error) {
     console.log(error);
     return res.status(500).json({ message: "Something went wrong" });
   }
 };
-
 
 const addNovel = async (req, res) => {
   const { novelId } = req.params;
@@ -96,97 +90,157 @@ const addNovel = async (req, res) => {
 
     const alreadyExists = await UserNovel.findOne({
       novel: novelId,
-      user: req.user.id
+      user: req.user.id,
     });
 
     if (alreadyExists) {
       return res.status(409).json({ message: "Novel already in your list" });
     }
 
-    const forbiddenStatus = ["Reading", "Completed", "On Hold", "Dropped"]
-    if (novel.publication.status === "Upcoming" && forbiddenStatus.includes(status)) {
+    let strictStatus = status ?? "Reading";
+    if(novel.publication.status === "Upcoming") {
+      strictStatus = "Plan To Read";
+    }
+
+    const upcomingForbiddenStatus = ["Reading", "Completed", "On Hold", "Dropped"];
+    if (status !== undefined &&
+      novel.publication.status === "Upcoming" &&
+      upcomingForbiddenStatus.includes(status)
+    ) {
       return res.status(400).json({
-        message: "This novel has not released yet, you cannot mark it as anything but Plan To Read"
+        message:
+          "This novel has not released yet, you cannot mark it as anything but Plan To Read",
       });
     }
 
-    if ((novel.publication.status === "Upcoming" ||
-      novel.publication.status === "Plan To Read") && (progress > 0 ||
-        rating !== null || startedAt !== null || completedAt !== null)) {
+    if (
+      (novel.publication.status === "Upcoming") &&
+      (progress !== undefined ||
+        rating !== undefined ||
+        startedAt !== undefined ||
+        completedAt !== undefined)
+    ) {
       return res.status(400).json({
-        message: `You cannot set progress, rating, or dates for an unreleased 
-        or Plan To Read novel`
+        message: `You cannot set progress, rating, or dates for an unreleased novel`,
       });
     }
 
-    if (typeof progress === "number" &&
+    let progressCount = progress;
+    if (
+      typeof progress === "number" &&
       novel.chapterCount > 0 &&
-      progress > novel.chapterCount) {
-      progress = novel.chapterCount;
+      progress > novel.chapterCount
+    ) {
+      progressCount = novel.chapterCount;
     }
 
     const userNovel = await UserNovel.create({
       user: req.user.id,
       novel: novelId,
-      status,
-      progress,
+      status: strictStatus,
+      progress: progressCount,
       rating,
       startedAt,
       completedAt,
     });
 
-    return res.status(200).json({
+    return res.status(201).json({
       message: "Novel added to your list",
-      userNovel
+      userNovel,
     });
-  }
-  catch (error) {
+  } catch (error) {
     return res.status(500).json({ message: "Something went wrong" });
   }
-}
-
+};
 
 const editNovel = async (req, res) => {
-  const { status, chapter, startedAt, completedAt } = req.body;
-  const { novelId } = req.params;
-  const userId = req.user.id;
+  const { userNovelId } = req.params;
+  const { status, progress, rating, startedAt, completedAt } = req.body;
 
-  if (status === "Completed" && !totalChapter) {
-    return res.status(400).json({
-      message: "Total Chapter required when reading status is completed"
-    });
-  }
-
-  if (status === "Completed" && chapter > totalChapter) {
-    return res.status(400).json({
-      message: "Read chapter cannot be more than Total Chpater"
-    });
+  if (!userNovelId) {
+    return res.status(400).json({ message: "Novel id is required" });
   }
 
   try {
-    const updatedNovel = await UserNovel.findOneAndUpdate(
-      { user: userId, _id: novelId },
-      { chapter, totalChapter, status, startedAt, completedAt },
-      { new: true }
-    ).lean();
+    const userNovel = await UserNovel.findOne({ _id: userNovelId, user: req.user.id });
+    if (!userNovel) {
+      return res.status(404).json({ message: "Novel not found" });
+    }
 
-    if (!updatedNovel) {
-      return res.status(404).json({
-        message: "Novel not found"
+    const novelId = userNovel.novel;
+
+    const novel = await GlobalNovel.findById(novelId);
+    if(!novel) {
+      return res.status(404).json({ message: "Novel not found" });
+    }
+
+    const upcomingForbiddenStatus = ["Reading", "Completed", "On Hold", "Dropped"];
+    if (status !== undefined &&
+      novel.publication.status === "Upcoming" &&
+      upcomingForbiddenStatus.includes(status)
+    ) {
+      return res.status(400).json({
+        message:
+          "This novel has not released yet, you cannot mark it as anything but Plan To Read",
       });
     }
 
+    if (
+      (novel.publication.status === "Upcoming") &&
+      (progress !== undefined ||
+        rating !== undefined ||
+        startedAt !== undefined ||
+        completedAt !== undefined)
+    ) {
+      return res.status(400).json({
+        message: `You cannot set progress, rating, or dates for an unreleased novel`,
+      });
+    }
+
+    let progressCount = progress
+    if (
+      typeof progress === "number" &&
+      novel.chapterCount > 0 &&
+      progress > novel.chapterCount
+    ) {
+      progressCount = novel.chapterCount;
+    }
+
+    const allowed_fields = [
+      "status",
+      "progress",
+      "rating",
+      "startedAt",
+      "completedAt",
+    ];
+
+    const updates = {};
+    for (const key of allowed_fields) {
+      if (req.body[key] !== undefined) {
+        updates[key] = key === "progress" ? progressCount : req.body[key];
+      }
+    }
+
+    const updatedEntry = await UserNovel.findOneAndUpdate(
+      {
+        _id: userNovelId,
+        user: req.user.id,
+      },
+      updates,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
     return res.status(200).json({
       message: "Novel updated successfully",
-      novel: updatedNovel
+      updatedEntry,
     });
-  }
-  catch (error) {
-    console.log(error);
+  } catch (error) {
     return res.status(500).json({ message: "Something went wrong" });
   }
-}
-
+};
 
 const deleteNovel = async (req, res) => {
   try {
@@ -200,7 +254,7 @@ const deleteNovel = async (req, res) => {
 
     const result = await UserNovel.deleteOne({ _id: novelId, user: userId });
     if (result.deletedCount === 0) {
-      return res.status(404).json({ message: "Novel not found" });
+      return res.status(404).json({ message: "Cannot delete the novel" });
     }
 
     return res.status(200).json({ message: "Novel deleted successfully" });
@@ -210,5 +264,11 @@ const deleteNovel = async (req, res) => {
   }
 };
 
-
-export { viewNovel, getUserNovel, searchNovelByName, addNovel, editNovel, deleteNovel };
+export {
+  viewNovel,
+  getUserNovel,
+  searchNovelByName,
+  addNovel,
+  editNovel,
+  deleteNovel,
+};
--- src/validators/user-novel.validator.js
+++ src/validators/user-novel.validator.js
@@ -3,28 +3,30 @@ import { z } from "zod";
 export const userNovelValidator = z.object({
 
     progress:
-    z.number().min(0).optional(),
+        z.number().min(0).optional(),
 
-    rating:
-    z.number().min(1)
-     .max(10)
-     .multipleOf(0.5)
-     .optional(),
+    rating: z
+        .number()
+        .min(1)
+        .max(10)
+        .multipleOf(0.5)
+        .nullable()
+        .optional(),
 
-    readingStatus:
-    z.enum(["Reading", "Completed", 
-        "On Hold", "Plan To Read", "Dropped"])
+
+    status:
+        z.enum(["Reading", "Completed", "On Hold", "Plan To Read", "Dropped"])
         .default("Reading"),
 
     startedAt:
-    z.coerce.date().optional(),
+        z.coerce.date().nullable().optional(),
 
     completedAt:
-    z.coerce.date().optional(),
+        z.coerce.date().nullable().optional(),
 
 }).refine(
     (data) => {
-        if(data.readingStatus === "Completed") {
+        if (data.status === "Completed") {
             return data.completedAt != null;
         }
         return true;
