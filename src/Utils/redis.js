52d7e3cc1e6ad6e4993134c74b8ecf12d7906e45
unknown <priyanshu123543@gmail.com>
2025-12-31 13:01:12 +0530

    Add bootstrap and config files

---
+++ src/Utils/redis.js
@@ -0,0 +1,14 @@
+import { createClient } from "redis";
+
+const redisClient = createClient({ 
+    url: "redis://localhost:6379"
+});
+
+redisClient.on("error", (err) => {
+    console.error("Redis error", err);
+});
+
+await redisClient.connect();
+console.log("Redis connected successfully");
+
+export { redisClient };
